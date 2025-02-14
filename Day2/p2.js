bdd4c0a02d69df5b252fef1abfefe257f0ff1bba
Kavya-cgit <mailing2chinka@gmail.com>
2025-02-14 12:16:03 +0530

    Data on server

---
+++ Day2/p2.js
@@ -0,0 +1,10 @@
+const http=require('http');
+const server = http.createServer((req,res) => {
+    res.writeHead(200,{'content-type':'text/html'});
+    res.end(`<h1 style="color:red ;">hello world</h1>`);
+});
+server.listen(9000, (err) =>{
+    if(err)
+        console.log(err);
+    console.log("Server is running at http://localhost:9000/");
+});
---
+++ Day2/p4.js
@@ -0,0 +1,20 @@
+const http=require("http");
+const User=[
+    {id:1,name:"nish",age:25},
+    {id:2,name:"amy",age:28},
+    {id:3,name:"sheldon",age:30},
+
+
+]
+
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{"content-type":"application/json"});
+    const name=User.map((user)=>{
+        return user.name;
+    })
+    res.end(JSON.stringify(User));
+})
+
+server.listen(9001,()=>{
+    console.log('server is running at http://localhost:9001/');
+});
